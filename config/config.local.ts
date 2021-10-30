1d8f69cc5c8a16693ee3176944b29695dd14ca58
Euclid-Twin <wdashuang@gmail.com>
2021-10-30 21:57:57 +0800

    update conig

--- config/config.local.ts
+++ config/config.local.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+    // define: {
+    //     'process.env.RPC_NODE_1': 'https://rpc-mumbai.maticvigil.com',
+
+    //     'process.env.RPC_NODE_2': 'https://rpc-mumbai.maticvigil.com',
+
+    //     'process.env.RPC_NODE_3': 'https://rpc-mumbai.maticvigil.com',
+
+    //     'process.env.APP_CHAIN_ID': '80001', //'97',
+    // },
     define: {
-        'process.env.RPC_NODE_1': 'https://rpc-mumbai.maticvigil.com',
+        'process.env.RPC_NODE_1':
+            'https://data-seed-prebsc-1-s1.binance.org:8545/',
 
-        'process.env.RPC_NODE_2': 'https://rpc-mumbai.maticvigil.com',
+        'process.env.RPC_NODE_2':
+            'https://data-seed-prebsc-2-s1.binance.org:8545/',
 
-        'process.env.RPC_NODE_3': 'https://rpc-mumbai.maticvigil.com',
+        'process.env.RPC_NODE_3':
+            'https://data-seed-prebsc-1-s2.binance.org:8545/',
 
         'process.env.APP_CHAIN_ID': '80001', //'97',
     },
