e44865874224408282390ed084923fd453235ca8
Euclid-Twin <wdashuang@gmail.com>
2021-10-30 21:44:46 +0800

    update config

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  define: {
-    'process.env.RPC_NODE_1': 'https://bsc-dataseed.binance.org/',
+    define: {
+        'process.env.RPC_NODE_1':
+            'https://data-seed-prebsc-1-s1.binance.org:8545/',
 
-    'process.env.RPC_NODE_2': 'https://bsc-dataseed1.defibit.io/',
+        'process.env.RPC_NODE_2':
+            'https://data-seed-prebsc-2-s1.binance.org:8545/',
 
-    'process.env.RPC_NODE_3': 'https://bsc-dataseed1.ninicoin.io/',
+        'process.env.RPC_NODE_3':
+            'https://data-seed-prebsc-1-s2.binance.org:8545/',
 
-    'process.env.APP_CHAIN_ID': '137',
-  },
+        'process.env.APP_CHAIN_ID': '80001', //'97',
+    },
 });
--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'umi';
 import routes from './routes';
 export default defineConfig({
-  nodeModulesTransform: {
-    type: 'none',
-  },
-  routes,
-  fastRefresh: {},
-  locale: {},
+    nodeModulesTransform: {
+        type: 'none',
+    },
+    routes,
+    fastRefresh: {},
+    locale: {},
+    links: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+            crossorigin: true,
+        },
+        {
+            href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap',
+            rel: 'stylesheet',
+        },
+    ],
 });
